6634ae29dada828dcab9a14074fb24eacc61d89b
System Administrator <root@0587425366.wireless.umich.net>
2019-11-19 13:29:26 -0500

    uploaded the first version of SoundBox

---
+++ PAT443VIME/Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    public GameObject cubeSoundPrefab;
+    public GameObject cylinderSoundPrefab;
+    public GameObject capsuleSoundPrefab;
+
+    public GameObject soundParent;
+    public GameObject cubeSoundObj;
+    public GameObject cylinderSoundObj;
+    public GameObject capsuleSoundObj;
+
+    public GameObject modulePrefab;
+    public GameObject moduleParent;
+    public GameObject ui;
+
+    readonly Color FILTER_MODULE_COLOR = Color.red;
+    readonly Color VOLUMN_UP_MODULE_COLOR = Color.black;
+    readonly Color VOLUMN_DOWN_MODULE_COLOR = Color.white;
+    readonly Color PITCH_UP_MODULE_COLOR = Color.blue;
+    readonly Color PITCH_DOWN_MODULE_COLOR = Color.green;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void resetCubeSound()
+    {
+        // destroy old sound;
+        Destroy(cubeSoundObj);
+
+        // instantiate new cube sound
+        cubeSoundObj = Instantiate(cubeSoundPrefab, soundParent.transform);
+        cubeSoundObj.name = "CubeSound";
+    }
+
+    public void resetCylinderSound()
+    {
+        // destroy old sound;
+        Destroy(cylinderSoundObj);
+
+        // instantiate new cylinder sound
+        cylinderSoundObj = Instantiate(cylinderSoundPrefab, soundParent.transform);
+        cubeSoundObj.name = "CylinderSound";
+    }
+
+    public void resetCapsuleSound()
+    {
+        // destroy old sound;
+        Destroy(capsuleSoundObj);
+
+        // instantiate new capsule sound
+        capsuleSoundObj = Instantiate(capsuleSoundPrefab, soundParent.transform);
+        cubeSoundObj.name = "CapsuleSound";
+    }
+
+    #region Module Instantiate
+    public void InstantiateFilterModule()
+    {
+        // Instantiate the module
+        GameObject newModule = Instantiate(modulePrefab, moduleParent.transform.position, moduleParent.transform.rotation, moduleParent.transform);
+        // Change Color
+        newModule.GetComponent<Renderer>().material.color = FILTER_MODULE_COLOR;
+        newModule.name = "FilterModule";
+    }
+
+    public void InstantiateVolumeUpModule()
+    {
+        // Instantiate the module
+        GameObject newModule = Instantiate(modulePrefab, moduleParent.transform.position, moduleParent.transform.rotation, moduleParent.transform);
+        // Change Color
+        newModule.GetComponent<Renderer>().material.color = VOLUMN_UP_MODULE_COLOR;
+        newModule.name = "VolumeUpModule";
+    }
+
+    public void InstantiateVolumeDownModule()
+    {
+        // Instantiate the module
+        GameObject newModule = Instantiate(modulePrefab, moduleParent.transform.position, moduleParent.transform.rotation, moduleParent.transform);
+        // Change Color
+        newModule.GetComponent<Renderer>().material.color = VOLUMN_DOWN_MODULE_COLOR;
+        newModule.name = "VolumeDownModule";
+    }
+
+    public void InstantiatePitchUpModule()
+    {
+        // Instantiate the module
+        GameObject newModule = Instantiate(modulePrefab, moduleParent.transform.position, moduleParent.transform.rotation, moduleParent.transform);
+        // Change Color
+        newModule.GetComponent<Renderer>().material.color = PITCH_UP_MODULE_COLOR;
+        newModule.name = "PitchUpModule";
+    }
+
+    public void InstantiatePitchDownModule()
+    {
+        // Instantiate the module
+        GameObject newModule = Instantiate(modulePrefab, moduleParent.transform.position, moduleParent.transform.rotation, moduleParent.transform);
+        // Change Color
+        newModule.GetComponent<Renderer>().material.color = PITCH_DOWN_MODULE_COLOR;
+        newModule.name = "PitchDownModule";
+    }
+    #endregion
+
+
+    #region Helper Functions
+    /// <summary>
+    /// return the average val of two colors
+    /// </summary>
+    /// <param name="moduleColor"></param>
+    /// <param name="soundColor"></param>
+    /// <returns></returns>
+    static public Color GetMergedColor(Color soundColor, Color moduleColor)
+    {
+        return new Color((moduleColor.r + soundColor.r)/2, (moduleColor.g + soundColor.g)/2, (moduleColor.b + soundColor.b)/2);
+    }
+
+    #endregion
+}
---
+++ PAT443VIME/Assets/Scripts/SoundCollider.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollider : MonoBehaviour
+{
+    public float SIZE_INCREMENT_X = 0.1f;
+    public float SIZE_INCREMENT_Y = 0.1f;
+    public float SIZE_INCREMENT_Z = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "FilterModule":
+                // change the color and the size
+                GetComponent<Renderer>().material.color = InteractionManager.GetMergedColor(GetComponent<Renderer>().material.color, other.gameObject.GetComponent<Renderer>().material.color);
+                transform.parent.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+                transform.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+
+                // TODO: edit audio
+
+                // destroy the module
+                Destroy(other.gameObject);
+                break;
+            case "VolumeUpModule":
+                // change the color and the size
+                GetComponent<Renderer>().material.color = InteractionManager.GetMergedColor(GetComponent<Renderer>().material.color, other.gameObject.GetComponent<Renderer>().material.color);
+                transform.parent.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+                transform.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+
+                // TODO: edit audio
+
+                // destroy the module
+                Destroy(other.gameObject);
+                break;
+            case "VolumeDownModule":
+                // change the color and the size
+                GetComponent<Renderer>().material.color = InteractionManager.GetMergedColor(GetComponent<Renderer>().material.color, other.gameObject.GetComponent<Renderer>().material.color);
+                transform.parent.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+                transform.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+
+                // TODO: edit audio
+
+                // destroy the module
+                Destroy(other.gameObject);
+                break;
+            case "PitchUpModule":
+                // change the color and the size
+                GetComponent<Renderer>().material.color = InteractionManager.GetMergedColor(GetComponent<Renderer>().material.color, other.gameObject.GetComponent<Renderer>().material.color);
+                transform.parent.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+                transform.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+
+                // TODO: edit audio
+
+                // destroy the module
+                Destroy(other.gameObject);
+                break;
+            case "PitchDownModule":
+                // change the color and the size
+                GetComponent<Renderer>().material.color = InteractionManager.GetMergedColor(GetComponent<Renderer>().material.color, other.gameObject.GetComponent<Renderer>().material.color);
+                transform.parent.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+                transform.localScale += new Vector3(SIZE_INCREMENT_X, SIZE_INCREMENT_Y, SIZE_INCREMENT_Z);
+
+                // TODO: edit audio
+
+                // destroy the module
+                Destroy(other.gameObject);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
